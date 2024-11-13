53c79e485b3dd06aabdc79a387864d3d727ab30e
XMG <xmg2004@outlook.com>
2024-11-13 22:41:22 +0800

    lab 8 -lab9的6.6

---
+++ 331-backend/src/main/java/se331/lab/rest/dao/aboutorganizer/OrganizerDao.java
@@ -0,0 +1,12 @@
+package se331.lab.rest.dao.aboutorganizer;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.lab.rest.entity.Organizer;
+
+import java.util.Optional;
+
+public interface OrganizerDao {
+    Page<Organizer> getOrganizer(Pageable pageRequest);
+    Optional<Organizer> findById(Long id);
+}
--- 331-backend/src/main/java/se331/lab/rest/repository/OrganizerRepository.java
+++ 331-backend/src/main/java/se331/lab/rest/repository/OrganizerRepository.java
@@ -1,7 +1,10 @@
 package se331.lab.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import se331.lab.rest.entity.Organizer;
+
+import java.util.List;
 
 public interface OrganizerRepository extends JpaRepository<Organizer,Long > {
 
